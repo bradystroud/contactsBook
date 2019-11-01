83c972f5c004ce66bf15720f64adfd55f292e221
MrChooky <jeynesbrook@gmail.com>
2019-11-01 10:12:14 +1000

    Issues fixed and code made nicer
    - Fixed the issue with the commit() function not saving the data to the database
    
    - Removed redundant code and made the code look nicer and more efficient

--- contactBook.py
+++ contactBook.py
@@ -1,68 +1,74 @@
+#
+# COMMAND LINE CONTACT BOOK
+# MADE BY 
+# 
+# CONTRIBUTERS:
+# BROOK JEYNES, 
+#
+# APPLICATION DESCRIPTION:
+#
+#
+#
+#
+
+# IMPORTS
 import sqlite3
 from sqlite3 import Error
 
+# INITIALISES DATABASE VARIABLES
 con = sqlite3.connect('contacts.db')
 cursorObj = con.cursor()
 
-
-def sql_connection():
-    try:
-        con = sqlite3.connect('contacts.db')
-        print("Connection is established: Database is created on path")
-        return con
-    except Error:
-        print(Error)
- 
-sql_connection()
+# CREATES SQL TABLE IF NOT ALREADY CREATED
 def sql_table(con):
     cursorObj = con.cursor()
-    cursorObj.execute(''' CREATE TABLE if not exists contacts(
-                                     cID INTEGER PRIMARY KEY,
-                                     fname TEXT,
-                                     lname TEXT,
-                                     phone TEXT,
-                                     email TEXT
-                                 )''')
+    cursorObj.execute(""" 
+    CREATE TABLE if not exists contacts(
+        cID INTEGER PRIMARY KEY,
+        fname TEXT,
+        lname TEXT,
+        phone TEXT,
+        email TEXT
+    )
+    """)
  
     con.commit()
-con = sql_connection()
 
-
-
-
-
+# MAIN FUNCTION RUN
 def main(con):
-
+    # GET CONTACT VALUES AND INSERT THEM INTO DATABASE
     def add_contact(con):
+        sql_insert = """
+        INSERT INTO contacts (fname, lname, phone, email) 
+        VALUES (?,?,?,?)
+        """
+
         fname = input("enter first name: ")
         lname = input("enter last name: ")
         phone = input("enter number: ")
         email = input("enter email: ")
 
-        cursorObj.execute(""" INSERT INTO contacts
-                                (
-                                    fname, lname, phone, email
-                                )
-                                VALUES
-                                (
-                                    ?,?,?,?
-                                ); """, (fname, lname, phone, email))
-        con = sql_connection()
+        cursorObj.execute(sql_insert, (fname, lname, phone, email))
         con.commit()
-        print("contact "+fname+" "+lname+" added")
 
-        cursorObj.execute("SELECT * FROM contacts")
-        rows = cursorObj.fetchall()
-        for row in rows:
-            print(row)
-        con.commit()
-        q = input("add another?(y/n): ")
-        if q == "y":
+        # CONFIRMATION MESSAGE
+        print("contact", fname, lname, "added")
+
+        # SHOWS ALL CONTACTS ADDED // COMMENTED OUT BECAUSE IT THE ADDITION OF IT MADE NO SENSE TO ME, BROOK .J
+        #cursorObj.execute("SELECT * FROM contacts")
+        #rows = cursorObj.fetchall()
+        #for row in rows:
+        #    print(row)
+
+        # GIVES USER CHOICE TO ADD ANOTHER CONTACT
+        userChoice = input("add another?(y/n): ")
+        if userChoice == "y":
             add_contact(con)
+            con.commit()
         else:
             con.commit()
-            main(con)
 
+    # DISLAYS ALL CONTACTS IN BOOK
     def ls():
         cursorObj.execute("SELECT * FROM contacts")
         rows = cursorObj.fetchall()
@@ -77,11 +83,7 @@ def main(con):
         rows = cursorObj.fetchall()
         for row in rows:
             deta = input("which detail would you like to change for "+str(row)+"?: ")
-        cursorObj.execute("UPDATE")
-
-
-        
-
+        cursorObj.execute("UPDATE")   
 
     def norm_cmd():
         if cmd == "a":
@@ -95,13 +97,8 @@ def main(con):
         elif cmd == "ls":
             ls()
         else:
-            print()
-            print("Hmmm... that doesn't seem like a command, enter 'help' to see all commands")
+            print("\nHmmm... that doesn't seem like a command, enter 'help' to see all commands")
             main(con)
-
-    
-
-
 
     cmd = input("enter command letter: ")
     if cmd == "help":
@@ -109,25 +106,25 @@ def main(con):
     else:
         norm_cmd()
 
-
+# GIVES USER A RUNDOWN OF HOW TO USE THE COMMAND LINE APPLICATION
 def help():
-    print("Type 'a' for instructions")
-    print("Type 's' to search for a contact")
-    print("Type 'ls' to list all contacts")
-    print("Type 'd' to delete a contact")
-    input("Press Enter to continue...")
-    print()
-    print()
+    print("""
+    Type 'a' for instructions
+    Type 's' to search for a contact
+    Type 'ls' to list all contacts
+    Type 'd' to delete a contact
+    Press Enter to continue...\n
+    """)
     main(con)
 
-
+# WELCOME MESSAGES
 def one():
     c="_"
     c=c.center(10)
     print(c*54)
-    print("Welcome to your contact book")
-    print()
-    print()
+    print("Welcome to your contact book\n\n")
     print("Type 'help' for instructions")
     main(con)
+
+# APPLICATION STARTPOINT
 one()
